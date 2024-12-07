112e09f85881ebf7b399c27bb2851eb425537728
Jayvee Salango <jayvee.salango10@gmail.com>
2024-12-08 02:44:57 +0800

    disabled crsf token for /api routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('/api/admin/login', 'AuthController@adminLogin');
+
+Route::post('api/admin/login', [AuthController::class, 'adminLogin']);
+
 
